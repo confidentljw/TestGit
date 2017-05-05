cac2aeffb55d040db785114c75e7dedfab3895c2
mv <confident_ljwsts@163.com>
2017-05-05 14:33:29 +0800

    提交github

---
+++ src/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.print("");
+        System.out.println("提交git");
+    }
+}
